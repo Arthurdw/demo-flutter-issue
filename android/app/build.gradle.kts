77c29d7f4afce003747c3c702d92e1eeb3bb1d41
Arthurdw <dev@arthurdw.com>
2025-04-03 21:46:20 +0200

    begin: demo

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "app.demo"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_23
+        targetCompatibility = JavaVersion.VERSION_23
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_23.toString()
+    }
+
+    defaultConfig {
+        applicationId = "app.demo"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+        buildToolsVersion = "34.0.0";
+        sdkArgs = {
+          buildToolsVersions = [ buildToolsVersion "28.0.3" ];
+          platformVersions = [ "34" "28" ];
+          # abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          abiVersions = [ "x86_64" ];
+          includeSystemImages = true;
+          includeEmulator = "if-supported";
+          extraLicenses = [
+            "android-sdk-preview-license"
+            "android-googletv-license"
+            "android-sdk-arm-dbt-license"
+            "google-gdk-license"
+            "intel-android-extra-license"
+            "intel-android-sysimage-license"
+            "mips-android-sysimage-license"
+          ];
+        };
+        androidComposition = pkgs.androidenv.composeAndroidPackages sdkArgs;
+        androidEmulator = pkgs.androidenv.emulateApp {
+          name = "android-sdk-emulator";
+          configOptions = { "hw.keyboard" = "yes"; };
+          sdkExtraArgs = sdkArgs;
+        };
+        androidSdk = androidComposition.androidsdk;
+        platformTools = androidComposition.platform-tools;
+        emulator_name = "demo-app";
+        jdk = pkgs.jdk23;
+      in {
+        devShells.default = pkgs.mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+          JAVA_HOME = "${jdk}/lib/openjdk";
+
+          buildInputs = with pkgs; [
+            # For more packages/package search go to https://search.nixos.org/
+            flutter
+            jdk
+            gradle
+
+            # Android SDK
+            androidSdk
+            platformTools
+            androidEmulator
+          ];
+
+          shellHook = ''
+            # create emulator if it doesn't exist
+            if ! avdmanager list avd | grep -q "${emulator_name}"; then
+              echo "Creating emulator ${emulator_name}..."
+              flutter emulator --create --name ${emulator_name}
+            fi
+          '';
+        };
+      });
+}
